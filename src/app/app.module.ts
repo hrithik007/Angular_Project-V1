b977f69637ff9f34c10d916883ad8ee6b2f02e95
hrithik007 <hrithik.jais2013@gmail.com>
2021-04-30 17:29:50 +0530

    Header and Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,11 +13,18 @@ import 'hammerjs';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -29,7 +36,7 @@ import { MenuComponent } from './menu/menu.component';
     MatButtonModule,
     MatCardModule
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
